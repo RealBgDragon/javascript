c5164ea33fb3d8f0dbea8499c68332951d0dc453
RealBgDragon <martin06200508@gmail.com>
2023-06-15 11:30:34 +0300

    a

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-let userName = "Martin";
+const playerText = $("#playerPick");
+const cpuText = $("#cpuPick");
+const resultText = $("#resultText");
+const choiseBtns = document.querySelectorAll(".my-button");
 
-let a = userName.length;
-let b = userName.charAt(3);
+let cpuPick;
+let playerPick;
 
-console.log(a);
-console.log(b);
+choiseBtns.forEach((button) =>
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        playerPick = button.textContent;
+        computerTurn();
+        playerText.textContent = "Player choise is: ${player}";
+    })
+);
 
-$("#myButton_res").text("Gay");
-document.getElementById("myButton_res").innerHTML = "not gay";
+function computerTurn() {
+    const randNum = Math.floor(Math.random() * 3 + 1);
+
+    switch (randNum) {
+        case 1:
+            cpuPick = "ROCK";
+            break;
+        case 2:
+            cpuPick = "PAPER";
+            break;
+        case 3:
+            cpuPick = "SCISSORS";
+            break;
+    }
+}
