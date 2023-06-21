baffab3b75160054fd71e63f34ddc631bab74ea6
RealBgDragon <128041836+RealBgDragon@users.noreply.github.com>
2023-06-21 23:20:06 +0300

    rock, paper sizzors

--- index.js
+++ index.js
@@ -5,28 +5,37 @@ const choiseBtns = document.querySelectorAll(".my-button");
 
 let cpuPick;
 let playerPick;
+let winner;
+
+const gameOptions = ["ROCK", "PAPER", "SCISSORS"];
+const gameRules = {
+    ROCK: "SCISSORS",
+    PAPER: "ROCK",
+    SCISSORS: "PAPER",
+};
 
 choiseBtns.forEach((button) =>
     button.addEventListener("click", (event) => {
         event.preventDefault();
         playerPick = button.textContent;
         computerTurn();
-        playerText.textContent = "Player choise is: ${player}";
+        $("#playerPick").text(`Player choise is: ${playerPick}`);
+        console.log(playerPick);
+        $("#cpuPick").text(`CPU choise is ${cpuPick}`);
+        console.log(`CPU ${cpuPick}`);
+        winner = Winner();
+        $("#resultText").text(`Winner is ${winner}`);
     })
 );
 
 function computerTurn() {
-    const randNum = Math.floor(Math.random() * 3 + 1);
+    cpuPick = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+}
 
-    switch (randNum) {
-        case 1:
-            cpuPick = "ROCK";
-            break;
-        case 2:
-            cpuPick = "PAPER";
-            break;
-        case 3:
-            cpuPick = "SCISSORS";
-            break;
+function Winner() {
+    if (playerPick === cpuPick) {
+        return "Draw";
+    } else {
+        return gameRules[playerPick] === cpuPick ? "Player" : "CPU";
     }
 }
