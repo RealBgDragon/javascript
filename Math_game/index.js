a7fdf6649f58ff1b8b353bdac1f9ff01715676d0
RealBgDragon <128041836+RealBgDragon@users.noreply.github.com>
2023-09-30 01:23:07 +0300

    added dark mode

--- Math_game/index.js
+++ Math_game/index.js
@@ -4,6 +4,7 @@ const check_answer = document.getElementById("checkAnswerBtn");
 const user_answer = document.getElementById("userAnswer");
 const user_feedback = document.getElementById("feedbackMsg");
 const dropdown = document.getElementById("difficulty");
+const themeToggle = document.getElementById("theme-toggle");
 
 let a, b;
 let difficulty;
@@ -51,3 +52,52 @@ check_answer.addEventListener("click", function () {
         user_feedback.classList.remove("correct");
     }
 });
+
+//check on load
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = checkCookie("theme");
+
+    if (savedTheme) {
+        if (savedTheme === "dark") {
+            document.body.classList.add("dark-mode");
+            themeToggle.textContent = "🌙";
+        } else {
+            document.body.classList.remove("dark-mode");
+            themeToggle.textContent = "☀️";
+        }
+    }
+});
+
+//light and dark mode
+themeToggle.addEventListener("click", function () {
+    let body = document.body;
+
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        themeToggle.textContent = "☀️"; // Sun icon for light mode
+        setCookie("theme", "light", 30); // for 30 days
+    } else {
+        body.classList.add("dark-mode");
+        themeToggle.textContent = "🌙"; // Moon icon for dark mode
+        setCookie("theme", "dark", 30);
+    }
+});
+//cockie
+function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function checkCookie(name) {
+    const theme = getCookie(name);
+    if (theme) {
+        return theme;
+    } else {
+        return false;
+    }
+}
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
