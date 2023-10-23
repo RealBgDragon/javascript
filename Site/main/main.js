f218c5c6cd0ce067a64134aaaf0527f7248870e2
RealBgDragon <martin06200508@gmail.com>
2023-10-23 14:15:22 +0300

    main page redo

--- Site/main/main.js
+++ Site/main/main.js
@@ -1,1 +1,16 @@
+// $(document).ready(function () {
+//     const listProducts = $(".list-products");
+//     const scrollAmount = 300; // Change this value to adjust the scrolling distance
+//     const animationDuration = 1900; // Just under 2 seconds for the animation
 
+//     setInterval(function () {
+//         const newScrollPosition = listProducts.scrollLeft() + scrollAmount;
+
+//         listProducts.stop().animate(
+//             {
+//                 scrollLeft: newScrollPosition,
+//             },
+//             animationDuration
+//         );
+//     }, 5000);
+// });
