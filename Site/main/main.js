6aad3abbae9b5f5d5364ea7e994c0d637089cea3
RealBgDragon <martin06200508@gmail.com>
2023-12-04 13:46:53 +0200

    Update main.js

--- Site/main/main.js
+++ Site/main/main.js
@@ -1,16 +1,21 @@
-// $(document).ready(function () {
-//     const listProducts = $(".list-products");
-//     const scrollAmount = 300; // Change this value to adjust the scrolling distance
-//     const animationDuration = 1900; // Just under 2 seconds for the animation
+$(document).ready(function () {
+    const listProducts = $(".list-products");
+    let scrollAmount = 300; // Change this value to adjust the scrolling distance
+    const animationDuration = 1900; // Just under 2 seconds for the animation
+    let timesScrolled = 0;
 
-//     setInterval(function () {
-//         const newScrollPosition = listProducts.scrollLeft() + scrollAmount;
-
-//         listProducts.stop().animate(
-//             {
-//                 scrollLeft: newScrollPosition,
-//             },
-//             animationDuration
-//         );
-//     }, 5000);
-// });
+    setInterval(function () {
+        const newScrollPosition = listProducts.scrollLeft() + scrollAmount;
+        timesScrolled++;
+        if (timesScrolled >= 2) {
+            timesScrolled = 0;
+            scrollAmount *= -1;
+        }
+        listProducts.stop().animate(
+            {
+                scrollLeft: newScrollPosition,
+            },
+            animationDuration
+        );
+    }, 5000);
+});
